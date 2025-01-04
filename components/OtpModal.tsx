99e535741dd8874904e1ea29baea7479b07582c4
Yunus Habeeb <habeebdh1@gmail.com>
2025-01-04 04:15:38 +0100

    Add authentication pages and UI components

---
+++ components/OtpModal.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OtpModal = ({
+  email,
+  accountId,
+}: {
+  email: string;
+  accountId: string;
+}) => {
+  return <div>OtpModal</div>;
+};
+
+export default OtpModal;
